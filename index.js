d2ebc980b1f5b8891a30ef33f00e505d501a0607
polarity <Charles.Ran9@gmail.com>
2024-01-17 22:40:01 -0500

    docs: added documentation for GameServer and socket handling

--- index.js
+++ index.js
@@ -17,11 +17,23 @@ const io = new Server(httpServer, {
 let queue = {}; // waiting queue after creating a game
 let rooms = {}; // game rooms
 
+/**
+ * Server class that implements the server state logic and handles websocket requests
+ * @param code The room code
+ * @param whitePlayer The User ID of the white player
+ * @param redPlayer The User ID of the red player
+ * @param turn A boolean indicating the turn (0 = white, 1 = red)
+ * @param turnTimestamp A timestamp storing the last time a change in turn occurred
+ * @param whiteTime A number storing the milliseconds left for player white
+ * @param redTime A number storing the milliseconds left to player red
+ * @param checkers A CheckersServer object storing the checkers game state
+ * @param gameEndPromises An array storing setTimeout Node.js Promises to end the game
+ * @param ended A boolean indicating if the game has ended or not
+ */
 class GameServer {
     code;
     whitePlayer;
     redPlayer;
-    moves;
     turn; // 0 = white, 1 = red
     turnTimestamp;
     whiteTime;
@@ -30,11 +42,18 @@ class GameServer {
     gameEndPromises;
     ended;
     
+    /**
+     * Constructor initializes the properties
+     * @param code The room code
+     * @param socket1 The User ID of the first player
+     * @param socket2 The User ID of the second player
+     * @param time The time control for both players, in minutes
+     */
     constructor(code, socket1, socket2, time){
         time = Math.min(time, 59);
         this.code = code;
 
-        // randomize side
+        // randomize side and assign IDs
         if (Math.random() > 0.5){
             this.whitePlayer = socket1;
             this.redPlayer = socket2;
@@ -43,12 +62,13 @@ class GameServer {
             this.whitePlayer = socket2;
         }
 
+        // Sets the time and initializes properties
         this.whiteTime = time * 60000; // minutes to ms
         this.redTime = time * 60000;
-        this.moves = [];
         this.turn = 1;
         this.checkers = new CheckersServer();
         this.turnTimestamp = Date.now();
+        // Start the timer for red
         this.gameEndPromises = [setTimeout(()=> {
             this.gameEnd(true, 1);
         }, this.redTime)];
@@ -67,7 +87,7 @@ class GameServer {
 
     /**
      * Check player color
-     * @param id socket id of the player
+     * @param id User ID of the player
      * @return 0 if invalid, 1 if white, 2 if red
      */
     getColor(id){
@@ -80,13 +100,21 @@ class GameServer {
         return 0;
     }
 
+    /**
+     * Attempts to move for white
+     * @param id User ID of the request
+     * @param from Coordinate to move from
+     * @param to Coordinate to move to
+     */
     moveWhite(id, from, to){
-        // not white's turn or wrong auth
+        // not white's turn or wrong ID authentication
         if (this.turn == 1 || id != this.whitePlayer || this.ended){
             return;
         }
 
+        // moves the piece
         if (this.checkers.move(true, from, to)){
+            // if moves successfully, changes the turn to red and start's red's timer
             this.clearTimeoutPromises();
             this.whiteTime -= (Date.now() - this.turnTimestamp);
             this.turnTimestamp = Date.now();
@@ -96,20 +124,30 @@ class GameServer {
             }, this.redTime));
         }
 
+        // send the new board state (potentially unmodified) to the clients
         this.emitState();
 
+        // check if game has actually ended, and if so, end the game
         if (this.checkers.findWinner()){
             this.gameEnd(this.checkers.findWinner() == 1, 0);
         }
     }
 
+    /**
+     * Attempts to move for red
+     * @param id User ID of the request
+     * @param from Coordinate to move from
+     * @param to Coordinate to move to
+     */
     moveRed(id, from, to){
-        // not red's turn or wrong auth
+        // not red's turn or wrong ID authentication
         if (this.turn == 0 || id != this.redPlayer || this.ended){
             return;
         }
 
+        // moves the piece
         if (this.checkers.move(false, from, to)){
+            // if moves successfully, changes the turn to white and start's white's timer
             this.clearTimeoutPromises();
             this.redTime -= (Date.now() - this.turnTimestamp);
             this.turnTimestamp = Date.now();
@@ -119,12 +157,19 @@ class GameServer {
             }, this.whiteTime));
         }
 
+        // send the new board state (potentially unmodified) to the clients
         this.emitState();
+
+        // check if game has actually ended, and if so, end the game
         if (this.checkers.findWinner()){
             this.gameEnd(this.checkers.findWinner() == 1, 0);
         }
     }
 
+    /**
+     * Stops all of the timers that if ended, 
+     * would trigger a gameEnd call with a loss for the player the timer started for
+     */
     clearTimeoutPromises(){
         for (let timeout of this.gameEndPromises){
             clearTimeout(timeout);
@@ -132,12 +177,23 @@ class GameServer {
         this.gameEndPromises = [];
     }
 
+    /**
+     * Ends the game
+     * @param whiteWin A boolean storing if white was the player that won
+     * @param reason A number storing the reason that the game ended
+     */
     gameEnd(whiteWin, reason){
+        // If already ended, method call is redudant
         if (this.ended){
             return;
         }
+
+        // Stop all the timers and end the game
         this.clearTimeoutPromises();
         this.ended = true;
+
+        // Log the reason the game ended: either a player took all the pieces,
+        // or someone ran out of time or disconnected
         if (reason == 0){
             console.log((whiteWin ? "WHITE":"RED") + " player took all the pieces");
         } else if (reason == 1){
@@ -146,10 +202,13 @@ class GameServer {
             console.log((whiteWin ? "RED":"WHITE") + " player disconnected");
         }
         console.log('Ended Game: ' + this.code);
+
+        // Emit to the clients that the game has ended and announce the winner
         io.to(this.code).emit('game-over', {
             winner: whiteWin ? this.whitePlayer:this.redPlayer
         });
 
+        // Disconnect the sockets from the room
         io.in(this.code).socketsLeave(this.code);
     }
 }
@@ -158,11 +217,21 @@ function generateCode(){
     return uuidv4();
 }
 
+/**
+ * Websocket connection handling
+ */
 io.on("connection", (socket) => {
+    /**
+     * Requests a room creation
+     * @param time Time control for the room
+     */
     socket.on("create-room", async ({time})=> {
+        // Validates the time is a valid positive integer
         if (!(Number.isInteger(time) && time > 0)){
             return;
         }
+
+        // Generates a UUID room code and waits in the queue for a challenger
         let code = generateCode();
         queue[code] = {
             socket1: socket.id,
@@ -170,11 +239,17 @@ io.on("connection", (socket) => {
             time
         };
 
+        // Make the websocket join the room and emit that a room is created to the client
         await socket.join(code);
         socket.emit('created-room', {code});
     });
 
+    /**
+     * Requests a room joining
+     * @param code Code for the room
+     */
     socket.on("join-room", async ({code}) => {
+        // Validates that the room exists
         if (!queue.hasOwnProperty(code)){
             socket.emit("join-room-failure", {
                 error: "Room Code Invalid"
@@ -184,6 +259,7 @@ io.on("connection", (socket) => {
 
         let {socket1, joinable, time} = queue[code];
 
+        // Validates that the room is still joinable
         if (!joinable){
             socket.emit("join-room-failure", {
                 error: "Room Full"
@@ -191,8 +267,11 @@ io.on("connection", (socket) => {
             return;
         }
 
+        // Start a GameServer in the room
         queue[code].joinable = false;
         rooms[code] = new GameServer(code, socket1, socket.id, time);
+
+        // Make the websockets join the room and emit that the game has started
         await socket.join(code);
         io.to(code).emit('init-game');
         io.to(rooms[code].whitePlayer).emit("start-game", {
@@ -209,15 +288,25 @@ io.on("connection", (socket) => {
         console.log("Started Game: " + code);
     });
 
+    /**
+     * Requests a move in the game
+     * @param code Room code of the request
+     * @param from Coordinate to move from
+     * @param to Coordinate to move to
+     */
     socket.on("move", ({code, from, to})=> {
+        // Check that room exists
         if (!code || !rooms[code]){
             return;
         }
+
+        // Check which color the request is made from
         let color = rooms[code].getColor(socket.id);
         if (!color){
             return;
         }
 
+        // Attempt to fulfill the move request in the room
         if (color == 1){
             rooms[code].moveWhite(socket.id, from, to);
         } else if (color == 2){
@@ -225,12 +314,16 @@ io.on("connection", (socket) => {
         }
     });
 
+    /**
+     * Handles a client disconnecting
+     */
     socket.on("disconnecting", ()=> {
         for (let room of socket.rooms){
             if (room == socket.id){
                 continue;
             }
 
+            // For each room the websocket is in, end the game and make the opponent win
             let color = rooms[room].getColor(socket.id);
             rooms[room].gameEnd(color == 2, 2);
             socket.leave(room);
