bc4f83570a7017e9673a85fb802523619da80426
polarity <Charles.Ran9@gmail.com>
2024-01-17 23:00:41 -0500

    test: add tests for checkers game logic

---
+++ checkers.test.js
@@ -0,0 +1,36 @@
+const checkers = require('./checkers');
+
+const {CheckersServer} = checkers;
+
+const checkersTest = new CheckersServer(
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 3, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+);
+
+test('taking a piece works', ()=> { 
+    checkersTest.move(false, [4, 6], [2, 4]);
+    expect(checkersTest.getBoard()).toEqual(
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 3, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    )
+});
+
+test('cannot make a not promoted piece a king', ()=> { 
+    expect(checkersTest.makeKing([2, 4])).toEqual(false)
+});
+
+test('white has no pieces therefore loses', ()=> { 
+    expect(checkersTest.findWinner()).toEqual(2)
+});
